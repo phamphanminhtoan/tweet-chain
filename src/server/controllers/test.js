91b7caaf0524da5d71561a04e5be6d1af05aaea5
khoaace <khoaace@gmail.com>
2018-12-17 23:54:31 +0700

    update tendermint

--- src/server/controllers/test.js
+++ src/server/controllers/test.js
@@ -7,19 +7,18 @@
       res.status(400).json(err);
     }
 }; */
-
 let { RpcClient } = require('tendermint');
-
 let client = RpcClient('wss://komodo.forest.network:443');
 
+
 // request a block
-
-
-
-
 exports.test = async (req, res) => {
     const params = req.body ? req.body : req.query;
-    client.block({ height: 1 })
-  .then((result) => {res.send(result);});
+    client.subscribe({
+      query: "tm.event='NewBlock'"
+    }, (err, event) => {
+      console.log(err);
+      console.log(event);
+    });
 };
 
