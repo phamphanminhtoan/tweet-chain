4522bd4388de94f233631f7ce01d3bc1f4a27a3c
phamphanminhtoan <phamphanminhtoan@gmail.com>
2018-12-23 09:25:17 +0700

    setup config

---
+++ src/client/config/energyValue.js
@@ -0,0 +1,5 @@
+export const BANDWIDTH_PERIOD = 86400;   //Chu ky bien doi 
+export const MAX_BLOCK_SIZE = 22020096;  /*kich thuoc toi da*/ 
+export const RESERVE_RATIO = 1;    //He so du tru
+export const MAX_CELLULOSE = Number.MAX_SAFE_INTEGER;    //So cel toi da
+export const NETWORK_BANDWIDTH = RESERVE_RATIO * MAX_BLOCK_SIZE * BANDWIDTH_PERIOD;    //Nang luong he thong
