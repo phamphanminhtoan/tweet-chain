a50c44144f2dd2c44fc12f9940309a35b6494826
phamphanminhtoan <phamphanminhtoan@gmail.com>
2018-12-24 15:06:34 +0700

    handle update v.1

---
+++ src/client/helpers/handle/update.js
@@ -0,0 +1,50 @@
+const v1 = require('./../lib/tx/v1');
+const hdlTransaction = require('./../lib/tx/index');
+const fs = require('fs');
+
+function encodeUpdateNameTransaction(publicKey, privateKey, name, sequence) {
+    return new Promise((resolve, reject) => {
+        const tx = {
+            version: 1,
+            operation: "update_account",
+            params: {
+                keys: "name",
+                value: Buffer.from(name, 'utf-8')
+            },
+            account: publicKey,
+            sequence,
+            memo: Buffer.alloc(0),
+        }
+        hdlTransaction.sign(tx, privateKey);
+        const txEncode = "0x" + hdlTransaction.encode(tx).toString('hex');
+        resolve(txEncode);
+    }, err => {
+        reject(err);
+    });
+}
+
+function encodeUpdatePictureTransaction(publicKey, privateKey, picturePath, sequence) {
+    return new Promise((resolve, reject) => {
+        const tx = {
+            version: 1,
+            operation: "update_account",
+            params: {
+                keys: "picture",
+                value: fs.readFileSync(picturePath)
+            },
+            account: publicKey,
+            sequence,
+            memo: Buffer.alloc(0),
+        }
+        hdlTransaction.sign(tx, privateKey);
+        const txEncode = "0x" + hdlTransaction.encode(tx).toString('hex');
+        resolve(txEncode);
+    }, err => {
+        reject(err);
+    });
+}
+
+module.exports = {
+    encodeUpdateNameTransaction,
+    encodeUpdatePictureTransaction
+}
--- src/client/helpers/te.js
+++ src/client/helpers/te.js
@@ -9,31 +9,35 @@ const axios = require('axios');
 // //     .then((res) => console.log(res))
 // //     .catch((err) => console.log(err))
  publicKey = 'GC25E5EAF7BVHT7CLN5YPFAPTUUWZOKCAKQTHTRE6N4J6BGGLMSHFHBR'
- var post = {
-    type: 1,
-    text: "Phu Thu"
-}
-//var content = new Buffer.from(JSON.stringify(post))
-//console.log(content)
-//var temp = JSON.parse(content.toString());
+//  var post = {
+//     type: 1,
+//     text: "Phu Thu"
+// }
+// //var content = new Buffer.from(JSON.stringify(post))
+// //console.log(content)
+// //var temp = JSON.parse(content.toString());
 
-// //console.log(temp)
+// // //console.log(temp)
+
+let buffPicture = fs.readFileSync('./a.JPEG'); 
+var buffName = Buffer.from("Toan Pham Phan Minh", 'utf-8');
+console.log(buff.toString())
 crawTx = {
     version: 1,
     account: publicKey,
-    sequence: 10,
+    sequence: 11,
     memo: Buffer.alloc(0),
-    operation: 'post',
+    operation: 'update_account',
     params: {
-        content : post
-        , keys: []
+        key: "name", //picture
+        value: Buffer.from("Toan Pham Phan Minh", 'utf-8')
     }
 }
 
-transaction.sign(crawTx, 'SDW6S4Q6Q5MTIT3R5C43QADD47OLXB2MEPYJ6WGBEOGLL57PJ3GA6HGE');
+ transaction.sign(crawTx, 'SDW6S4Q6Q5MTIT3R5C43QADD47OLXB2MEPYJ6WGBEOGLL57PJ3GA6HGE');
 
-var res = transaction.encode(crawTx).toString('hex');
-//console.log(res)
+ var res = transaction.encode(crawTx).toString('hex');
+console.log(res)
 
 // // var res2 = transaction.decode(Buffer.from('ATAdxPY3gelauaEcMyjMIuaJsiFe9bIO5kZTCil6jcMzJAwoAAAAAAAAAC8QQ1RUNTIyLUNRMjAxNS8zMgIAKzBwvd1bmTYX9+UH7/ClM9OijVUfiNNCJt7s89bHsXbP1FwWAAAAAAX14QAXKI+NeDuxq6oGCIz/LWFzLd/7Gv9IZyyK+l1IA6G0LPAOzJPdoAtFws8fcp326fNHVuO0qS6WvfT/FXyxph8D', 'base64'))
 // // console.log(res2);
@@ -65,38 +69,38 @@ var res = transaction.encode(crawTx).toString('hex');
 //     }
 //     return amount;
 // }
- var tx = 'ATC10nSAL8NTz+Jbe4eUD50pbLlCAqEzziTzeJ8ExlskcpwxAAAAAAAAAAoAAwANAAoBAAdQaHUgVGh1AJEbR6bt253Edv2QYyH9P8JK7e4BupkzZWdwIPvGzP3kde9MeDJnJss3stsWF1fFYqFryU8gFFJyGLJme34Poww='
+ var tx = 'ATAdxPY3gelauaEcMyjMIuaJsiFe9bIO5kZTCil6jcMzJAwoAAAAAAAAAK8ABAANBG5hbWUABktoYSBEb+inO45MmlHPDohDeU3DYmKtBnBRooYUtsFhEybhQpQhNAA7SUBxOQNYnhx3jMZv4WgpJwroflRSYygBbyUSdgE='
  tx = v1.decode(Buffer.from(tx, 'base64'))
- //console.log(tx);
+ console.log(tx.params.value.toString());
  //console.log(tx.params.content.toString());
  //tx.params.content = v1.decode(Buffer.from(tx.params.content, 'base64'))
 
  
-var TransactionFromPublicNode = "https://komodo.forest.network/tx_search?query=%22account=%27GC25E5EAF7BVHT7CLN5YPFAPTUUWZOKCAKQTHTRE6N4J6BGGLMSHFHBR%27%22";
-    axios.get(TransactionFromPublicNode)
-    .then((response) => {
-        //console.log(response.header)
-        const data = response.data.result.txs.map((each) => {
-            each.tx = v1.decode(Buffer.from(each.tx, 'base64'));
+// var TransactionFromPublicNode = "https://komodo.forest.network/tx_search?query=%22account=%27GC25E5EAF7BVHT7CLN5YPFAPTUUWZOKCAKQTHTRE6N4J6BGGLMSHFHBR%27%22";
+//     axios.get(TransactionFromPublicNode)
+//     .then((response) => {
+//         //console.log(response.header)
+//         const data = response.data.result.txs.map((each) => {
+//             each.tx = v1.decode(Buffer.from(each.tx, 'base64'));
             
-            each.tx.memo = each.tx.memo.toString();
-            each.tx.signature = each.tx.signature.toString('hex');
-            if (each.tx.params.content !== undefined){
-                var t = JSON.parse(each.tx.params.content.toString());
-                //console.log(t)
-            }
-            console.log(each)
-            return each;
-        });
+//             each.tx.memo = each.tx.memo.toString();
+//             each.tx.signature = each.tx.signature.toString('hex');
+//             if (each.tx.params.content !== undefined){
+//                 var t = JSON.parse(each.tx.params.content.toString());
+//                 //console.log(t)
+//             }
+//             console.log(each)
+//             return each;
+//         });
         
-       var  amount= calculateAmount(data, "GC25E5EAF7BVHT7CLN5YPFAPTUUWZOKCAKQTHTRE6N4J6BGGLMSHFHBR");
-       //console.log(amount)
+//        var  amount= calculateAmount(data, "GC25E5EAF7BVHT7CLN5YPFAPTUUWZOKCAKQTHTRE6N4J6BGGLMSHFHBR");
+//        //console.log(amount)
 
-    //    const account = Account.findByPk(tx.account, { transaction: dbTransaction });
-    // if (!account) {
-    //   throw Error('Account does not exists');
-    // }
-    })
-    .catch(error => {
-        console.log(error);
-    });
+//     //    const account = Account.findByPk(tx.account, { transaction: dbTransaction });
+//     // if (!account) {
+//     //   throw Error('Account does not exists');
+//     // }
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     });
