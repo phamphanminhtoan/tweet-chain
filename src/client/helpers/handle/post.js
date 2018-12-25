5d802f7cb6e67bd4fa1f9634b44b66c0de9ea570
phamphanminhtoan <phamphanminhtoan@gmail.com>
2018-12-25 20:34:23 +0700

    change some stuff

--- src/client/helpers/handle/post.js
+++ src/client/helpers/handle/post.js
@@ -2,24 +2,22 @@ const v1 = require('./../lib/tx/v1');
 const hdlTransaction = require('./../lib/tx/index');
 
 function encodePostTransaction(publicKey, privateKey, content, sequence) {
-    return new Promise((resolve, reject) => {
-        const tx = {
-            version: 1,
-            operation: "post",
-            params: {
-                keys: [],
-                content: content,
-            },
-            account: publicKey,
-            sequence,
-            memo: Buffer.alloc(0),
-        }
-        hdlTransaction.sign(tx, privateKey);
-        const txEncode = "0x" + hdlTransaction.encode(tx).toString('hex');
-        resolve(txEncode);
-    }, err => {
-        reject(err);
-    });
+
+    const tx = {
+        version: 1,
+        operation: "post",
+        params: {
+            keys: [],
+            content: content,
+        },
+        account: publicKey,
+        sequence,
+        memo: Buffer.alloc(0),
+    }
+    hdlTransaction.sign(tx, privateKey);
+    const txEncode = "0x" + hdlTransaction.encode(tx).toString('hex');
+    return txEncode;
+
 }
 
 module.exports = {
--- src/client/helpers/handle/update.js
+++ src/client/helpers/handle/update.js
@@ -3,24 +3,21 @@ const hdlTransaction = require('./../lib/tx/index');
 const fs = require('fs');
 
 function encodeUpdateNameTransaction(publicKey, privateKey, name, sequence) {
-    return new Promise((resolve, reject) => {
-        const tx = {
-            version: 1,
-            operation: "update_account",
-            params: {
-                keys: "name",
-                value: Buffer.from(name, 'utf-8')
-            },
-            account: publicKey,
-            sequence,
-            memo: Buffer.alloc(0),
-        }
-        hdlTransaction.sign(tx, privateKey);
-        const txEncode = "0x" + hdlTransaction.encode(tx).toString('hex');
-        resolve(txEncode);
-    }, err => {
-        reject(err);
-    });
+    const tx = {
+        version: 1,
+        operation: "update_account",
+        params: {
+            keys: "name",
+            value: Buffer.from(name, 'utf-8')
+        },
+        account: publicKey,
+        sequence,
+        memo: Buffer.alloc(0),
+    }
+    hdlTransaction.sign(tx, privateKey);
+    const txEncode = "0x" + hdlTransaction.encode(tx).toString('hex');
+    return txEncode;
+
 }
 
 function encodeUpdatePictureTransaction(publicKey, privateKey, picturePath, sequence) {
