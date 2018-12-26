e955c86097402a1529b4c2cb39af434eeacc7244
phamphanminhtoan <phamphanminhtoan@gmail.com>
2018-12-26 10:32:12 +0700

    handle followings

--- src/client/helpers/handle/update.js
+++ src/client/helpers/handle/update.js
@@ -1,6 +1,12 @@
 const v1 = require('./../lib/tx/v1');
 const hdlTransaction = require('./../lib/tx/index');
 const fs = require('fs');
+const base32 = require('base32.js');
+const vstruct = require('varstruct');
+
+const Followings = vstruct([
+    { name: 'addresses', type: vstruct.VarArray(vstruct.UInt16BE, vstruct.Buffer(35)) },
+]);
 
 function encodeUpdateNameTransaction(publicKey, privateKey, name, sequence) {
     const tx = {
@@ -35,10 +41,32 @@ function encodeUpdatePictureTransaction(publicKey, privateKey, picturePath, sequ
     hdlTransaction.sign(tx, privateKey);
     const txEncode = "0x" + hdlTransaction.encode(tx).toString('hex');
     return txEncode;
+}
+
+function encodeUpdateFollowingsTransaction(publicKey, privateKey, address, sequence) {
+    var following = {
+        addresses: [base32.decode(address)]
+    }
+    var followParams = Buffer.from(Followings.encode(following));
+    const tx = {
+        version: 1,
+        operation: "update_account",
+        params: {
+            key: "followings",
+            value: followParams
+        },
+        account: publicKey,
+        sequence,
+        memo: Buffer.alloc(0),
+    }
+    hdlTransaction.sign(tx, privateKey);
+    const txEncode = "0x" + hdlTransaction.encode(tx).toString('hex');
+    return txEncode;
 
 }
 
 module.exports = {
     encodeUpdateNameTransaction,
-    encodeUpdatePictureTransaction
+    encodeUpdatePictureTransaction,
+    encodeUpdateFollowingsTransaction
 }
--- src/client/helpers/te.js
+++ src/client/helpers/te.js
@@ -11,35 +11,41 @@ const pay = require('./handle/payment');
 const post = require('./handle/post')
 const up = require('./handle/update')
 const int = require('./handle/interact')
-
+const vstruct = require('varstruct');
 
 const publicKeyToan = 'GC25E5EAF7BVHT7CLN5YPFAPTUUWZOKCAKQTHTRE6N4J6BGGLMSHFHBR';
 const secretKeyToan = 'SDW6S4Q6Q5MTIT3R5C43QADD47OLXB2MEPYJ6WGBEOGLL57PJ3GA6HGE';
 
-// var tx = 'ATC10nSAL8NTz+Jbe4eUD50pbLlCAqEzziTzeJ8ExlskcpwxAAAAAAAAABQABQAkGxcK6JOx+jeq80d4D1n+J0sc4t5N/otdQRX9aavkuTcAAgIFCTZGuEBbTEANuRk6rmnba/gNRUimqGgTJyPVh3SPdhTP4CqLzCC4M9ptSIPmzuPfEIvmTaTyGy9BKRNP2JJYAg=='
-// tx = v1.decode(Buffer.from(tx, 'base64'))
+var tx = 'ATDWu4CH2zGdowQnEtrh97wGDp8eSyq6foVuU1x8Q6ipEpB7AAAAAAAAAHYABAFKCmZvbGxvd2luZ3MBPQAJMNo/ZGPHZmIkYzWx9dAXloXqb+hLIXZX/DtfayT/IVICtxkwPItpLrMfyxkeXgGf+y9hYSSC/MLSt0HWKLtS9F5Bm10i3zAdxPY3gelauaEcMyjMIuaJsiFe9bIO5kZTCil6jcMzJAwoMI90kfPf2VQLvSUUXvFODREy3pde8BlipuOIr6VdgQFYeHMwSd+DwYbJNJiRLkoum8CABCim8FeDGydd4A0Tia3QG3ggSzBQN5uBLKaZR3PD0m5LkdMJm/qeKruE1QYfMp2ubOB6iXIhMC9V4TTDR+gox1cKZnOXVun/qTjvKzolqHRwCTqOPZ3tw/AwFXqhe+iX+EQlQC4sMcCvEqHuA7GjhMWSs2KbX/Ff9eQBxTClkGdXvfF7QIQRu0eTeLFJKJtRZ88FLBBW6xW7H1EzTYtbOCiz3fkF5mAwg/3Z0TyC7kwA5TWAZlTgdiJ1NTYU56kqZwEygUM2nPLZ77UPNQarkbEMvArLf5b1xcLx94+lCA=='
+tx = v1.decode(Buffer.from(tx, 'base64'))
 
-// var txR = 'ATCPdJHz39lUC70lFF7xTg0RMt6XXvAZYqbjiK+lXYEBWHhzAAAAAAAAAJ8ABQAkmM9F48J3as+eaVWaq/wYGaz1iR6ep8ls+bnkzMLMimEAAgIF8pW/ctMTyl32+8U6W+CAMxKAC49venhjRm1XhWrL3f2jDGL1muumsah6Sj+2ZXa0fBO54bBM3sJ5/75ErH1NCA=='
-// txR = v1.decode(Buffer.from(txR, 'base64'))
-// console.log(tx);
+var txR = 'ATCPdJHz39lUC70lFF7xTg0RMt6XXvAZYqbjiK+lXYEBWHhzAAAAAAAAAJ8ABQAkmM9F48J3as+eaVWaq/wYGaz1iR6ep8ls+bnkzMLMimEAAgIF8pW/ctMTyl32+8U6W+CAMxKAC49venhjRm1XhWrL3f2jDGL1muumsah6Sj+2ZXa0fBO54bBM3sJ5/75ErH1NCA=='
+txR = v1.decode(Buffer.from(txR, 'base64'))
+//console.log(tx.params.value.toString());
+// const Followings = vstruct([
+//     { name: 'addresses', type: vstruct.VarArray(vstruct.UInt16BE, vstruct.Buffer(35)) },
+// ]);
 
-// const Rtx = {
-//         version: 1,
-//         operation: "interact",
-//         params: {
-//             object: '1B170AE893B1FA37AAF347780F59FE274B1CE2DE4DFE8B5D4115FD69ABE4B937',
-//             content: {
-//                 type: 1,
-//                 text: "Ngo "
-//             },
-//         },
-//         account: publicKeyToan,
-//         sequence: 20,
-//         memo: Buffer.alloc(0),
-//     }
-//     transaction.sign(Rtx, secretKeyToan);
-//     const txEncode = "0x" + transaction.encode(Rtx).toString('hex');
+// var f = 'GBVHFBAB6A225WXXT2BGM7REANIGJ5WXV3MU3OWZNYZ7WGUNXSYLTYIK'
+// var following = {
+//     addresses: [base32.decode(f)]
+// }
+// var updateParams = Buffer.from(Followings.encode(following));
+// const rtx = {
+//     version: 1,
+//     operation: "update_account",
+//     params: {
+//         key: "followings",
+//         value: updateParams
+//     },
+//     account: publicKeyToan,
+//     sequence: 22,
+//     memo: Buffer.alloc(0),
+// }
 
-//     console.log(txEncode)
+// transaction.sign(rtx, secretKeyToan);
+// const txEncode = "0x" + transaction.encode(rtx).toString('hex');
 
-console.log(int.encodeInteractTransaction(publicKeyToan, secretKeyToan, '1B170AE893B1FA37AAF347780F59FE274B1CE2DE4DFE8B5D4115FD69ABE4B937',4, 22))
+//console.log(txEncode)
+
+console.log(up.encodeUpdateFollowingsTransaction(publicKeyToan, secretKeyToan, f, 23))
